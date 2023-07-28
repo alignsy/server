e577831580e5dc0873e49959f5d1e2dca5a1a11f
Anukul Pandey <anukul.030601@gmail.com>
2023-07-28 16:54:25 +0530

    feat: removed cors

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, request, jsonify
 import requests
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
 url = "https://script.google.com/macros/s/AKfycby76jWCa-RJC8dPYtV6Rz8w_IRTHEC-lryvf8JIthu3iCiCrwpLFUTcZP5zGsV4v2AzOQ/exec"
 
 @app.route('/')

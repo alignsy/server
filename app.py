670e2ce064c41c043ea71eea2faf5350dfd939c7
Anukul Pandey <anukul.030601@gmail.com>
2023-07-28 16:46:52 +0530

    feat: home page ping

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 from flask import Flask, request, jsonify
 import requests
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 url = "https://script.google.com/macros/s/AKfycby76jWCa-RJC8dPYtV6Rz8w_IRTHEC-lryvf8JIthu3iCiCrwpLFUTcZP5zGsV4v2AzOQ/exec"
 
-@app.route('/api/join-waitlist', methods=['POST'])
+@app.route('/')
+def home():
+    return "hello home!"
+
+@app.route('/join-waitlist', methods=['POST'])
 def join_waitlist():
     requests.post(url, json={
         'email': request.json.get("email")
     })
     return jsonify(data="success"), 200
 
-@app.errorhandler(405)
-def method_not_allowed(error):
-    return jsonify(error='Method not allowed'), 405
 
 if __name__ == '__main__':
     app.run()
